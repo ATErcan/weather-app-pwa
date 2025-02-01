5e565e38935f5ff3e1dcec5d615f76f4adba0c94
talha <atercan0658@gmail.com>
2025-02-01 11:24:25 +0100

    [feature/add-new-forecast] - wrap the WeatherCard component with Link

--- src/components/WeatherCard.tsx
+++ src/components/WeatherCard.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import Image from "next/image";
 
 import { OPEN_WEATHER_API_ICONS_URL } from "@/lib/constants/api";
@@ -6,7 +7,7 @@ import { WeatherCondition, WeatherConditionImage } from "@/lib/types/weather.typ
 import Clock from "./Clock";
 
 export default function WeatherCard({ data }: IWeatherCardProps) {
-  const { name, weather, main: { temp, temp_max, temp_min }, dt, timezone } = data;
+  const { coord: { lat, lon }, name, weather, main: { temp, temp_max, temp_min }, dt, timezone } = data;
   const { main, icon } = weather[0];
 
   const bgImage = WeatherConditionImage[data.weather[0].main];
@@ -17,33 +18,36 @@ export default function WeatherCard({ data }: IWeatherCardProps) {
   
   return (
     <section
-      className={`flex justify-between px-3 py-2 h-24 text-white rounded-2xl bg-cover bg-center`}
+      className={`h-24 text-white rounded-2xl bg-cover bg-center hover:opacity-80`}
       style={backgroundStyle}
     >
-      <div className="flex flex-col justify-between">
-        <div>
-          <h2 className="text-lg font-medium">{name}</h2>
-          <Clock dt={dt} timezone={timezone} />
+      <Link
+        href={`/location?lat=${lat}&lon=${lon}`}
+        className="flex justify-between px-3 py-2 rounded-2xl"
+      >
+        <div className="flex flex-col justify-between">
+          <div>
+            <h2 className="text-lg font-medium">{name}</h2>
+            <Clock dt={dt} timezone={timezone} />
+          </div>
+          <div className="flex items-center">
+            <Image
+              src={`${OPEN_WEATHER_API_ICONS_URL}${icon}@2x.png`}
+              alt={`${main} icon`}
+              width={25}
+              height={25}
+            />
+            <p className="text-sm">{WeatherCondition[main]}</p>
+          </div>
         </div>
-        <div className="flex items-center">
-          <Image
-            src={`${OPEN_WEATHER_API_ICONS_URL}${icon}@2x.png`}
-            alt={`${main} icon`}
-            width={25}
-            height={25}
-          />
-          <p className="text-sm">
-            {WeatherCondition[main]}
-          </p>
+        <div className="flex flex-col items-center justify-between">
+          <p className="text-3xl">{Math.round(temp)}°</p>
+          <div className="flex gap-2 text-sm">
+            <p>H: {Math.round(temp_max)}°</p>
+            <p>L: {Math.round(temp_min)}°</p>
+          </div>
         </div>
-      </div>
-      <div className="flex flex-col items-center justify-between">
-        <p className="text-3xl">{Math.round(temp)}°</p>
-        <div className="flex gap-2 text-sm">
-          <p>H: {Math.round(temp_max)}°</p>
-          <p>L: {Math.round(temp_min)}°</p>
-        </div>
-      </div>
+      </Link>
     </section>
   );
 }
